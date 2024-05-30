9d083093b1512ade566f351abe2c74ca810e1ab4
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-05-30 16:52:05 +0200

    test require JSON file name

--- test/model.test.js
+++ test/model.test.js
@@ -5,3 +5,9 @@ test('Model should be a class', () => {
     const model = new Model();
     expect(model).toBeInstanceOf(Model);
 });
+
+test('Instance of model should require JSON file name during instantiation', () => {
+    const fileName = 'data.json';
+    const model = new Model(fileName);
+    expect(model.fileName).toBe(fileName);
+});
